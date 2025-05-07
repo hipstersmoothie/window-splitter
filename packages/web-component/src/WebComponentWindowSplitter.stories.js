b98d82c9226ca24935b3705b84700a93c62707e9
Andrew Lisowski <lisowski54@gmail.com>
2025-05-06 23:15:01 -0700

    more stories

--- packages/web-component/src/WebComponentWindowSplitter.stories.js
+++ packages/web-component/src/WebComponentWindowSplitter.stories.js
@@ -1,4 +1,5 @@
 import { html } from "lit";
+import { spring } from "framer-motion";
 import { Panel, WindowSplitter, PanelResizer } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
@@ -75,6 +76,397 @@ export const AutosaveCollapsible = {
         class="panel-resizer"
       ></window-panel-resizer>
       <window-panel class="panel" id="2">Panel 2</window-panel>
+    </window-splitter>
+  `,
+};
+
+// TODO: Add dynamic constraints
+
+export const SimpleMin = {
+  render: () => html`
+    <window-splitter class="panel-group">
+      <window-panel min="100px" class="panel">Panel 1</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 2</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 3</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const SimpleMinMax = {
+  render: () => html`
+    <window-splitter class="panel-group">
+      <window-panel min="100px" max="200px" class="panel">Panel 1</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 2</window-panel>
+      <window-panel-resizer
+        size="20px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 3</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const SimpleConstraints = {
+  render: () => html`
+    <window-splitter class="panel-group">
+      <window-panel min="100px" max="50%" class="panel">Panel 1</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel class="panel">Panel 2</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const HorizontalLayout = {
+  render: () => html`
+    <window-splitter class="panel-group" orientation="horizontal">
+      <window-panel default="30%" min="20%" class="panel">Left</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="20%" class="panel">Middle</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel default="30%" min="20%" class="panel">Panel 3</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const VerticalLayout = {
+  render: () => html`
+    <window-splitter
+      class="panel-group"
+      orientation="vertical"
+      style="height: 322px"
+    >
+      <window-panel default="30%" min="20%" class="panel">Top</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="20%" class="panel">Middle</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel default="30%" min="20%" class="panel">Bottom</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const VerticalLayout2 = {
+  render: () => html`
+    <window-splitter
+      class="panel-group"
+      orientation="vertical"
+      style="height: calc(100vh - 100px)"
+    >
+      <window-panel default="200px" min="200px" class="panel">top</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel
+        min="200px"
+        collapsedSize="60px"
+        defaultCollapsed
+        collapsible
+        class="panel"
+      >
+        middle
+      </window-panel>
+    </window-splitter>
+  `,
+};
+
+export const NestedGroups = {
+  render: () => html`
+    <window-splitter
+      class="panel-group"
+      orientation="horizontal"
+      style="border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 12px; height: 400px"
+    >
+      <window-panel min="10%">1</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="10%">
+        <window-splitter orientation="vertical">
+          <window-panel min="10%">2-1</window-panel>
+          <window-panel-resizer
+            size="10px"
+            class="panel-resizer"
+          ></window-panel-resizer>
+          <window-panel min="10%">
+            <window-splitter orientation="horizontal">
+              <window-panel min="20%">2-2-1</window-panel>
+              <window-panel-resizer
+                size="10px"
+                class="panel-resizer"
+              ></window-panel-resizer>
+              <window-panel min="20%">2-2-2</window-panel>
+            </window-splitter>
+          </window-panel>
+        </window-splitter>
+      </window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="10%">3</window-panel>
+    </window-splitter>
+  `,
+};
+
+export const WithOverflow = {
+  render: () => html`
+    <window-splitter class="panel-group" style="height: 400px">
+      <window-panel min="200px">
+        <div
+          style="
+            overflow: auto;
+            padding: 40px;
+            height: 100%;
+            box-sizing: border-box;
+          "
+        >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi, eu
+            tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+        </div>
+      </window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="200px">
+        <div
+          style="
+            overflow: auto;
+            padding: 40px;
+            height: 100%;
+            box-sizing: border-box;
+          "
+        >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi, eu
+            tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+          <p>
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl. Sed euismod, nisl eget ultricies
+            ultrices, nunc nisi aliquam nisi, eu tincidunt nisl nisl eu nisl.
+            Sed euismod, nisl eget ultricies ultrices, nunc nisi aliquam nisi,
+            eu tincidunt nisl nisl eu nisl.
+          </p>
+        </div>
+      </window-panel>
+    </window-splitter>
+  `,
+};
+
+export const Collapsible = {
+  render: () => html`
+    <window-splitter class="panel-group">
+      <window-panel
+        class="panel"
+        min="100px"
+        collapsible
+        collapsedSize="60px"
+        style="border: 10px solid green; box-sizing: border-box"
+        .onCollapseChange=${(e) => console.log("COLLAPSE PASSIVE", e)}
+      >
+        Panel 1
+      </window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 2</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel
+        id="panel-3"
+        min="100px"
+        class="panel"
+        collapsible
+        .collapseAnimation=${{ duration: 1000, easing: "bounce" }}
+        collapsedSize="60px"
+        style="border: 10px solid blue; box-sizing: border-box"
+        collapsed=${true}
+        .onCollapseChange=${(newCollapsed, el) => {
+          el.collapsed = newCollapsed;
+        }}
+      >
+        Panel 3
+      </window-panel>
+    </window-splitter>
+  `,
+};
+
+const springFn = spring({
+  keyframes: [0, 1],
+  velocity: 1,
+  stiffness: 100,
+  damping: 10,
+  mass: 1.0,
+});
+
+export const CustomCollapseAnimation = {
+  render: () => html`
+    <window-splitter class="panel-group">
+      <window-panel
+        class="panel"
+        min="100px"
+        collapsible
+        collapsedSize="60px"
+        style="border: 10px solid green; box-sizing: border-box"
+      >
+        Panel 1
+      </window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 2</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel
+        class="panel"
+        style="border: 10px solid blue; box-sizing: border-box"
+        min="100px"
+        collapsible
+        collapsedSize="60px"
+        defaultCollapsed
+        .collapseAnimation=${{
+          easing: (t) => springFn.next(t * 1000).value,
+          duration: 1000,
+        }}
+      >
+        Panel 3
+      </window-panel>
+    </window-splitter>
+  `,
+};
+
+// imperative
+
+// conditional
+
+// complex conditional
+
+export const WithDefaultWidth = {
+  render: () => html`
+    <window-splitter class="panel-group" style="height: 400px">
+      <window-panel style="background-color: #333366"></window-panel>
+      <window-panel-resizer size="3px"></window-panel-resizer>
+      <window-panel
+        style="background-color: #ff3366"
+        default="100px"
+        min="100px"
+        max="400px"
+      >
+      </window-panel>
+    </window-splitter>
+  `,
+};
+
+export const StaticAtRest = {
+  render: () => html`
+    <window-splitter class="panel-group" style="height: 200px">
+      <window-panel min="100px" max="300px" class="panel" isStaticAtRest>
+        Panel 1
+      </window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 2</window-panel>
+      <window-panel-resizer
+        size="10px"
+        class="panel-resizer"
+      ></window-panel-resizer>
+      <window-panel min="100px" class="panel">Panel 3</window-panel>
     </window-splitter>
   `,
 };
--- packages/web-component/src/index.ts
+++ packages/web-component/src/index.ts
@@ -14,11 +14,14 @@ import {
   PanelData,
   Orientation,
   prepareSnapshot,
+  getCollapsiblePanelForHandleId,
+  OnResizeCallback,
 } from "@window-splitter/state";
 import { createContext } from "wc-context";
 import { withContext } from "wc-context/lit.js";
 import { html, LitElement, PropertyValues } from "lit";
 import { ContextConsumer } from "wc-context/controllers.js";
+import { property } from "lit/decorators.js";
 
 import "wc-context/context-provider.js";
 
@@ -28,6 +31,7 @@ import {
   measureGroupChildren,
   move,
   SharedPanelGroupProps,
+  SharedPanelProps,
 } from "@window-splitter/interface";
 
 let _id = 0;
@@ -54,8 +58,10 @@ function updateAttributes(
   }
 }
 
-function isBooleanPropTrue(value: string | undefined) {
-  return value === "true" || value === "";
+function isBooleanPropValue(value: string | undefined) {
+  if (value === "true" || value === "") return true;
+  if (value === "false") return false;
+  return undefined;
 }
 
 createContext("send");
@@ -190,6 +196,12 @@ customElements.define("window-splitter", WindowSplitter);
 export class Panel extends withContext(LitElement) {
   static observedContexts = ["send"];
 
+  public onCollapseChange?: (newCollapsed: boolean, el: Panel) => void;
+  public onResize?: OnResizeCallback;
+  public collapseAnimation?: SharedPanelProps<boolean>["collapseAnimation"];
+  @property({ type: Boolean, reflect: true })
+  collapsed?: boolean;
+
   contextConsumer = new ContextConsumer<GroupMachineContextValue>(
     this,
     "context",
@@ -211,21 +223,19 @@ export class Panel extends withContext(LitElement) {
       id: this.id,
       min: this.getAttribute("min") as Unit | undefined,
       max: this.getAttribute("max") as Unit | undefined,
-      collapsible: isBooleanPropTrue(this.getAttribute("collapsible")),
-      collapsed: isBooleanPropTrue(this.getAttribute("collapsed")),
+      collapsible: isBooleanPropValue(this.getAttribute("collapsible")),
+      collapsed: isBooleanPropValue(this.getAttribute("collapsed")),
       collapsedSize: this.getAttribute("collapsedSize") as Unit | undefined,
-      // onCollapseChange: this.getAttribute("onCollapseChange"), // TODO
-      collapseAnimation: this.getAttribute("collapseAnimation") as
-        | "ease-in-out"
-        | "bounce"
-        | "linear"
-        | { duration: number; easing: "ease-in-out" | "bounce" | "linear" },
-      // onResize: this.getAttribute("onResize"), // TODO
-      defaultCollapsed: isBooleanPropTrue(
+      onCollapseChange: this.onCollapseChange
+        ? { current: (e) => this.onCollapseChange?.(e, this) }
+        : undefined,
+      collapseAnimation: this.collapseAnimation,
+      onResize: this.onResize ? { current: this.onResize } : undefined,
+      defaultCollapsed: isBooleanPropValue(
         this.getAttribute("defaultCollapsed")
       ),
       default: this.getAttribute("default") as Unit | undefined,
-      isStaticAtRest: isBooleanPropTrue(this.getAttribute("isStaticAtRest")),
+      isStaticAtRest: isBooleanPropValue(this.getAttribute("isStaticAtRest")),
     });
   }
 
@@ -261,6 +271,28 @@ export class Panel extends withContext(LitElement) {
 
     if (!this.getPanelData()) {
       this.send({ type: "registerPanel", data: this.initPanel() });
+    }
+  }
+
+  updated(changedProperties: PropertyValues) {
+    if (changedProperties.has("collapsed")) {
+      const isControlled = this.getPanelData()?.collapseIsControlled;
+
+      if (isControlled) {
+        if (this.collapsed) {
+          this.send({
+            type: "collapsePanel",
+            panelId: this.id,
+            controlled: true,
+          });
+        } else {
+          this.send({
+            type: "expandPanel",
+            panelId: this.id,
+            controlled: true,
+          });
+        }
+      }
     }
   }
 
@@ -332,6 +364,8 @@ export class PanelResizer extends withContext(LitElement) {
         currentValue: panelBeforeHandle?.currentValue,
         groupSize: getGroupSize(this.contextConsumer.value),
       }),
+      tabIndex: 0,
+      // tabIndex: disabled ? -1 : 0,
       style: {
         cursor: this.contextConsumer.value
           ? getCursor(this.contextConsumer.value)
@@ -379,8 +413,24 @@ export class PanelResizer extends withContext(LitElement) {
       moveProps.onPointerDown(e);
     });
     this.addEventListener("keydown", (e) => {
-      // if (disabled) return; // TODO
       moveProps.onKeyDown(e);
+
+      const collapsiblePanel = getCollapsiblePanelForHandleId(
+        this.contextConsumer.value,
+        this.id
+      );
+
+      console.log("collapsiblePanel", collapsiblePanel, e);
+
+      if (e.key === "Enter" && collapsiblePanel) {
+        if (collapsiblePanel.collapsed) {
+          console.log("expandPanel", collapsiblePanel.id);
+          this.send({ type: "expandPanel", panelId: collapsiblePanel.id });
+        } else {
+          console.log("collapsePanel", collapsiblePanel.id);
+          this.send({ type: "collapsePanel", panelId: collapsiblePanel.id });
+        }
+      }
     });
 
     updateAttributes(this, this.getAttributes());
